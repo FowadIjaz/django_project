732d1266c3e14196d9bc4b1fdf7bcacf90f03070
FowadIjaz <63612037+FowadIjaz@users.noreply.github.com>
2021-06-20 00:39:06 -0400

    added heroku-django

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,5 @@ LOGIN_URL = 'login'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = os.environ.get
+
+django_heroku.settings(locals())
